3ef9aa01199e55a470a7837b8839657db5f5611b
Deadwalk Dominic <deadwalk@live.cn>
2021-03-12 23:30:21 +0800

    修改trim编译异常的问题

--- src/common/Configuration/Config.cpp
+++ src/common/Configuration/Config.cpp
@@ -35,8 +35,6 @@ namespace
         }
 
         _configOptions.emplace(optionName, optionKey);
-
-        //sLog->outError("> Config: Add '%s' - '%s'\n", optionName.c_str(), optionKey.c_str());
     }
 
     void ParseFile(std::string const& file)
@@ -56,7 +54,6 @@ namespace
             if (line.empty())
                 continue;
 
-            line = acore::String::Reduce(line);
             line = acore::String::Trim(line, in.getloc());
 
             // comments
@@ -72,8 +69,8 @@ namespace
             if (equal_pos == std::string::npos || equal_pos == line.length())
                 return;
 
-            auto entry = acore::String::Reduce(line.substr(0, equal_pos));
-            auto value = acore::String::Reduce(line.substr(equal_pos + 1));
+            auto entry = acore::String::Trim(line.substr(0, equal_pos), in.getloc());
+            auto value = acore::String::Trim(line.substr(equal_pos + 1), in.getloc());
 
             value.erase(std::remove(value.begin(), value.end(), '"'), value.end());
 
